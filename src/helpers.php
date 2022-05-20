a7849be3b3cbac2b4d2239a0aea543c09a78b83b
logico <logico@golden-supreme.com>
2022-05-20 17:20:55 +0800

    加入一些辅助工具类；

---
+++ src/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!function_exists('array_build')) {
+    function array_build($array, callable $callback)
+    {
+        $results = [];
+
+        foreach ($array as $key => $value) {
+            list($innerKey, $innerValue) = call_user_func($callback, $key, $value);
+
+            $results[$innerKey] = $innerValue;
+        }
+
+        return $results;
+    }
+}
+
